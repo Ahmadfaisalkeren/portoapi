9d742fe8c758bb1b573bc3e88b9ef12a1943963b
AK <maulidae.rizki@gmail.com>
2025-11-06 09:51:17 +0800

    Update about migration

--- database/migrations/2025_11_06_015022_alter_description_column_in_abouts_table.php
+++ database/migrations/2025_11_06_015022_alter_description_column_in_abouts_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('abouts', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->text('description');
-            $table->timestamps();
+        Schema::table('abouts', function (Blueprint $table) {
+            $table->text('description')->change();
         });
     }
 
@@ -24,6 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('abouts');
+        Schema::table('abouts', function (Blueprint $table) {
+            $table->string('description', 255)->change();
+        });
     }
 };
